ee1dd715e1cc3a0f1c710175b4be20cff0bed4da
marcomammi02 <marcomammi2002@gmail.com>
2022-10-24 14:18:05 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+FOR creo un loop che mi genera numeri(n) fino a 100;
+IF (n) / 3 dà resto 0:
+    -(n) = 'Fizz'
+ELSE IF (n) / 5 dà resto 0:
+    -(n) = 'Buzz'
+ELSE IF (n) / 3 && (n) / 5 danno resto 0:
+    -(n) = 'FizzBuzz'
+
+*/
