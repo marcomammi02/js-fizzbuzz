5dcc38d7311f64bb2de57a5f2e1d1d55e027c379
marcomammi02 <marcomammi2002@gmail.com>
2022-10-24 14:18:55 +0200

    Creating js logic

--- script.js
+++ script.js
@@ -8,3 +8,17 @@ ELSE IF (n) / 3 && (n) / 5 danno resto 0:
     -(n) = 'FizzBuzz'
 
 */
+
+
+
+for (let n = 1; n <= 100; n++) {
+    if ((n % 3 == 0) && (n % 5 == 0)) {
+        console.log('fizzbuzz');
+    }else if (n % 3 == 0) {
+        console.log('fizz');
+    }else if (n % 5 == 0) {
+        console.log('buzz');
+    }else {
+    console.log(n)
+    }
+}
